dada6658893f297ba2ad33972b9a94f04b99e2ec
cesther1 <catlover1115@gmail.com>
2016-09-02 10:08:05 -0700

    Added Files

---
+++ PyLesson_01/firstProg.py
@@ -0,0 +1,12 @@
+print ("I")
+print ("am")
+print ("awesome")
+print ("as")
+print ("heck")
+print ("\"Recycle.\"")
+
+print ('hello')
+
+print ("""Hello""")
+print ("\"Hello.\"")
+print ("""I am an awesome person today. Writing random words right now. I'm not very coherent.""")
