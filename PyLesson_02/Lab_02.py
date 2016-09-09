b5db83549b596bfbbc03a3f752ae2d857a305090
cesther1 <catlover1115@gmail.com>
2016-09-09 09:32:45 -0700

    ASCII ART
    It's supposed to be an 'H'

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,18 @@
+p = 6 * 8
+print ("6 multiplied by 8 is",p)
+
+Name = " Esther "
+Address = "Milkweed Road 3982 "
+City = "San Diego "
+ZipCode = "92130 "
+
+print (Name + Address + City + ZipCode)
+
+
+length = 2
+height = 4
+width = 6
+constant = 2
+Addition = length * height + width * height + length * width
+SurfaceArea = Addition * 2
+print ("The surface area is", SurfaceArea,)
---
+++ PyLesson_02/PYLesson_02.py
@@ -0,0 +1,19 @@
+name = "QueenEsther"
+print (name)
+print ("Bow to me")
+
+variableone = "\"Gates of Hell\" "
+variabletwo = "by Auguste Rodin"
+print (variableone + variabletwo)
+
+leeks = 5
+print ("I ate",leeks,"leeks and they were ALL disgusting.")
+
+l = 5
+
+w = 4
+
+RectArea = l * w
+
+print ("The area of your rectangle is", RectArea)
+
