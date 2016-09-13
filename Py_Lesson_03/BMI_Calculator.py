a311b44102ea54b639027b3ffed290ed5a2c9020
Esther Choi <catlover1115@gmail.com>
2016-09-13 09:59:04 -0700

    Revert "Lesson_3"
    This reverts commit b8e88383803aa72b2dc3bed0c1f41efe8d5dc3c0.

--- Py_Lesson_03/BMI_Calculator.py
+++
@@ -1,6 +0,0 @@
-print ("Hello. I will be finding your BMI.")
-
-num = int (input("How tall are you in meters?"))
-num2 = int (input("How much do you weigh in kilograms?"))
-calculation = 720 * (num2/(num**2))
-print ("Your BMI is", calculation)
--- Py_Lesson_03/Calculation.py
+++
@@ -1,5 +0,0 @@
-print ("Hello.")
-sibling = int(input("How many siblings do you have?"))
-pets = int(input("How many pets do you have?"))
-result = sibling / pets
-print ("You have" ,result , "siblings per pets.")
