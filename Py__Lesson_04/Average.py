61b9a0a50df42e97b307ee63ab3e28ea7f45f41c
Esther Choi <catlover1115@gmail.com>
2016-09-29 10:10:45 -0700

    Lesson_04

--- Py__Lesson_04/Average.py
+++ Py__Lesson_04/Average.py
@@ -2,9 +2,9 @@ num1 = float(input("Enter number 1:"))
 num2 = float(input("Enter number 2:"))
 num3 = float(input("Enter number 3:"))
 def average (num8, num5, num4):
-    return ((num1+num2+num3)/3)
+    return ((num8+num5+num4)/3)
 def printf (num8, num5, num4):
-    print ("The average of" ,num1, "," ,num2, ", and" ,num3,"is" , average(num8, num5, num4), ".")
+    print ("The average of" ,num1, "," ,num2, ", and" ,num3,"is {:10.5f}".format(average(num8, num5, num4)),".")
 
 average (num1, num2, num3) 
 printf (num1, num2, num3)
--- Py__Lesson_04/Circle.py
+++ Py__Lesson_04/Circle.py
@@ -1,15 +1,9 @@
 r = float(input("What is the radius of the circle?"))
 def calcArea (num1):
     return ((num1**2)*3.1415)
+def formatf (num1):
+    print ("The area of a circle with a radius of",r, "is {:10.5f}".format(calcArea(num1)))
+    return (num1)
+formatf (r)
 
-def printf (num1):
-    print ("The area of a circle with a radius of", calcArea(num1))
-calcArea (r)
-printf (r)
 
-Name = " Esther "
-Address = "Milkweed Road 3982 "
-City = "San Diego "
-ZipCode = "92130 "
-
-print (Name + Address + City + ZipCode)
