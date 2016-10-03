eba85ac2718955264ae368cf96cfcf15f9bf5067
Esther Choi <catlover1115@gmail.com>
2016-10-03 09:56:26 -0700

    Lesson_3

--- Py_Lesson_03/Calculation.py
+++ Py_Lesson_03/Calculation.py
@@ -1,5 +1,5 @@
 print ("Hello.")
 sibling = int(input("How many siblings do you have?"))
-pets = int(input("How many pets do you have?"))
-result = sibling / pets
-print ("You have" ,result , "siblings per pets.")
+slices = int(input("How many pizza slices do you have?"))
+result = sibling / slices
+print ("You have" ,result , "siblings per slice.")
