45baabc566b1ce778ff5287e30d35f12294c66c5
Esther Choi <catlover1115@gmail.com>
2016-10-07 10:22:36 -0700

    Lesson_04

---
+++ Py__Lesson_04/StudyGuide4.py
@@ -0,0 +1,35 @@
+#1.
+#item1 = float(input("please enter the cost of the first item:"))
+#item2 = float(input("please enter the cost of the second item:"))
+#def format (one, two):
+ #   print ("The total cost of your order is {:5.2}".format(item1+item2))
+#format (item1, item2)
+
+#2.
+
+#item1 = input("please enter the first item:")
+#price1 = float(input("please enter the cost of the first item:"))
+#item2 = input("please enter the second item:")
+#price2 = float(input("please enter the cost of the second item:"))
+#item3 = input("please enter the third item:")
+#price3 = float(input("please enter the cost of the third item:"))
+#item4 = input("please enter the fourth item:")
+#price4 = float(input("please enter the cost of the fourth item:"))
+#def formatf (one, two):
+    #print ("{:<12}\t ......{:3.2}".format(one, two))
+#formatf (item1, price1)
+#formatf (item2, price2)
+#formatf (item3, price3)
+#formatf (item4, price4)
+
+#3. D.
+num1 = 5
+num2 = 7
+num3 = 9
+numPrint (num1, num2, num3)
+def calcAvg (one, two, three):
+    return (own +two+three)/3
+def numPrint (one, two, three):
+    avg = calcAvg (one, two, three)
+    print ("The average of", one, two, "and",three,"is", avg)
+
