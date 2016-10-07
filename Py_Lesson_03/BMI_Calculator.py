4c8e209c99ab48221f649c2355d4d7e73ac9dd9c
Esther Choi <catlover1115@gmail.com>
2016-10-07 10:22:20 -0700

    Lesson_3

--- Py_Lesson_03/BMI_Calculator.py
+++ Py_Lesson_03/BMI_Calculator.py
@@ -1,7 +1,7 @@
 print ("Hello. I will be finding your BMI.")
 
-num = int (input("How tall are you in meters?"))
-num2 = int (input("How much do you weigh in kilograms?"))
-calculation = 720 * (num2/(num**2))
+num = int (input("How tall are you in inches?"))
+num2 = int (input("How much do you weigh in pounds?"))
+calculation = 703 * (num2/(num**2))
 print ("Your BMI is", calculation)
 
