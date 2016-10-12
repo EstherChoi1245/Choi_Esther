5339d196d761cc4a51128963c95146bcf90d287f
Esther Choi <catlover1115@gmail.com>
2016-10-12 10:22:53 -0700

    Lesson_04

---
+++ Py_Lesson_04/StudyGuide42.py
@@ -0,0 +1,10 @@
+item1 = float(input("Enter:"))
+
+def blah (num1):
+    output = num1+2
+    return output
+def printf (num1):
+    print ("NUmber is {:2.2f}".format(blah(num1)))
+
+blah (item1)
+printf (item1)
