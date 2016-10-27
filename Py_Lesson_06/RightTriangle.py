dc7772ced8fcb7c13cfb5d0fd142afc3ad512ad7
Esther Choi <catlover1115@gmail.com>
2016-10-27 10:24:26 -0700

    Lesson_06

---
+++ Py_Lesson_06/RightTriangle.py
@@ -0,0 +1,9 @@
+string = input ("Enter a word:")
+
+def printf():
+    for letter in range(len(string), 0, -1):
+        print (string[letter:len(string)])
+            
+printf()
+
+
---
+++ Py_Lesson_06/Song.py
@@ -0,0 +1,12 @@
+
+def loop(wd,nm):
+    out =""
+    for i in range(0, nm, 1):
+        out = out + wd + " "
+    print(out)
+
+    
+loop("Na",4)
+
+loop 
+        
