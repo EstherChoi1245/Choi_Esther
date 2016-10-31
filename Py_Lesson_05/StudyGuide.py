ca1f8d1a2ee628b3e1f20c0d4c8864680ac5a557
Esther Choi <catlover1115@gmail.com>
2016-10-31 10:23:22 -0700

    Lesson_05

---
+++ Py_Lesson_05/StudyGuide.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter a number"))
+num2 = int(input("Enter a number:"))
+total=int
+MAX= 3
+if total == MAX:
+    print ("total is equal  to MAX")
+if total < :
+    print ("total == MAX and is < sum.")
+else:
+    print ("total is not equal to MAX")
+    
