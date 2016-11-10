3ae8df51545cda3083883312ef85ee4f8b227c63
Esther Choi <catlover1115@gmail.com>
2016-11-10 10:22:31 -0800

    Lesson_08

--- Py_Lesson_05/Receipt.py
+++ Py_Lesson_05/Receipt.py
@@ -8,24 +8,33 @@ item4 = input("Please enter item 4:")
 price4 = float(input("Please enter the price for item 4:"))
 subtotal = price1+price2+price3+price4
 
-if subtotal>=2000: 
-    discount = subtotal*0.15
-if subtotal<2000:
-    discount = subtotal*0
+
 def formatf (num1, num2):
-    print ("{:<10}\t{:5.2}".format(num1, num2))
-       
+    print ("{:<10}{:0.2f}".format(num1, num2))
+
+def discountc():
+    global subtotal, discount
+    if subtotal>=2000: 
+        discount = subtotal*0.15
+    else:
+        discount = subtotal*0
+        
+        
+discountc()
+discount=0
+tax=subtotal*0.08
+total= subtotal-discount+tax
+
+print ("<<<<<<<<<<<< Receipt >>>>>>>>>>>>>>")
 formatf(item1, price1)
 formatf(item2, price2)
 formatf(item3, price3)
 formatf(item4, price4)
-subtotal = price1+price2+price3+price4
 
-print ("<<<<<<<<<<<< Receipt >>>>>>>>>>>>>>") 
-print ("Subtotal:" ,subtotal)
-print ("Tax:",subtotal*0.08)
-print ("Discount:",discount)
-print ("Total:", subtotal-discount+(subtotal*0.08))
+print (formatf("Subtotal:", subtotal))
+print (formatf("Discount:",discount))
+print (formatf("Tax:",tax))
+print (formatf("Total:", total))
 print ("___________________________")
 print ("Thank you!")
 
---
+++ Py_Lesson_08/TreeDeg60.py
@@ -0,0 +1,20 @@
+word = input("Enter a word:")
+stop = len(word)
+start = 0
+def tree (word, start, stop):
+    if start <= stop:
+        print ([0])
+        start+=1
+        tree (word, start, stop)
+        
+tree (word, start, stop)
+
+
+
+
+
+
+
+
+
+
