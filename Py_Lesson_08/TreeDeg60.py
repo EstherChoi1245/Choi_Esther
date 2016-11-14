bbf55028ea98b1738448e38a79db81fac2afb31f
Esther Choi <catlover1115@gmail.com>
2016-11-14 10:21:19 -0800

    Lesson_08

--- Py_Lesson_08/TreeDeg60.py
+++ Py_Lesson_08/TreeDeg60.py
@@ -3,11 +3,14 @@ stop = len(word)
 start = 0
 def tree (word, start, stop):
     if start <= stop:
-        print ([0])
+        print ("{:>6}".format(word[0:start]))
         start+=1
-        tree (word, start, stop)
+        tree (word, start, stop)        
+    else:
+        return 0 
         
 tree (word, start, stop)
+
 
 
 
