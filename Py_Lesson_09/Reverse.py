1585ec6f088542ba4be801a2443c307f1191bd68
Esther Choi <catlover1115@gmail.com>
2016-11-17 10:24:32 -0800

    Lesson_09

---
+++ Py_Lesson_09/Reverse.py
@@ -0,0 +1,22 @@
+words = ["tired", "extremely", "am", "I", "Hey,"]
+print ("In order....")
+myList = []
+
+output = " "
+j = 0
+for i in myList:
+    print (myList[i])
+    output += 1
+    
+print ("Reversed......")
+
+li= " "
+def reverse():
+    for r in range (len(words), 0, -1):
+        li = li + str(r)
+    
+reverse()
+
+#print 
+
+
