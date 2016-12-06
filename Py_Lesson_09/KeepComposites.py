b1867740970be6e77dff91ca4d7f44f7af9c3a28
Esther Choi <catlover1115@gmail.com>
2016-12-06 10:15:13 -0800

    Lesson_09

--- Py_Lesson_09/KeepComposites.py
+++ Py_Lesson_09/KeepComposites.py
@@ -3,14 +3,13 @@ nums= [2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79]
 def gFactor(number):
     for i in range(2, number):
         if number%i==0:
-            return 1
-        else:
-            return 0
+            return 1    
+    return 0
 
 def removePrimes(nums):
     for number in nums:
         if gFactor(number)==0:
-            return(nums.remove(number))
+            nums.remove(number)
 
 removePrimes(nums)
 print (nums)
