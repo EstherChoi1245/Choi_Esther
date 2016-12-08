086381af7a5307a2270ddbec447d81010dc657ca
Esther Choi <catlover1115@gmail.com>
2016-12-08 10:21:42 -0800

    Lesson_09

--- Py_Lesson_09/ExpressionSolver.py
+++ Py_Lesson_09/ExpressionSolver.py
@@ -1,26 +1,32 @@
-expression= input("Please enter a mathematical expression:")
-equation = [split(expression)]
-
-while i < len(equation):
-    if i < len(equation) and (equation[i].find( "*" or "/")):
-        if equation[i]== "*":
-            equation[i]== int(equation[i-1])*int(i+1)
-        else:
-            equation[i]== int(equation[i-1])/int(i+1)
-        equation.remove(equation[i-1])
-        equation.remove(equation[i])
-    i+=1
+expression= input("Please enter a mathematical expression with spaces between terms:")
+equation = expression.split(" ")
 
 i=0
 while i < len(equation):
-    if i <len(equation) and (equation[i].find("+" or "-")):
+    if i <len(equation) and equation[i] == "*" or "/":
+        if equation[i]== "*":
+            equation[i]=int(equation[i-1])*int(equation[i+1])
+            equation.pop(i+1)
+            equation.pop(i-1)
+        elif equation[i]== "/":
+            equation[i]= int(equation[i-1])/(int(equation[i+1]))
+            equation.pop(i+1)
+            equation.pop(i-1)        
+    i+= 1
+                    
+
+i=0
+while i < len(equation):
+    if i <len(equation) and (equation[i]== "+" or "-"):
         if equation[i]== "+":
             equation[i]=int(equation[i-1])+int(equation[i+1])
-        else:
-            equation[i]= int(equation[i-1])-int(equation[i+1])
-        equation.remove(equation[i-1])
-        equation.remove(equation[i]
-    i += 1
+            equation.pop(i-1)
+            equation.pop(i)
+        elif equation[i]== "-":
+            equation[i]= int(equation[i-1])-(int(equation[i+1]))
+            equation.pop(i-1)
+            equation.pop(i)        
+    i+= 1
                     
 print(equation)
 
