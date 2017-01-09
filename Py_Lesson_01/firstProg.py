09b487ccffd34ae45e62d0ada7119dc95f54a9fe
Esther Choi <catlover1115@gmail.com>
2017-01-09 10:24:19 -0800

    Lesson_01

--- Py_Lesson_01/firstProg.py
+++ Py_Lesson_01/firstProg.py
@@ -10,3 +10,6 @@ print ('hello')
 print ("""Hello""")
 print ("\"Hello.\"")
 print ("""I am an awesome person today. Writing random words right now. I'm not very coherent.""")
+
+list1= [1, 2, 3, 5]
+print(len(list1))
