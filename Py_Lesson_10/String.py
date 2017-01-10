e9f571de44157c6d175d3123a34ca0b685911bce
Esther Choi <catlover1115@gmail.com>
2017-01-10 10:25:01 -0800

    Lesson_10

--- Py_Lesson_10/String.py
+++ Py_Lesson_10/String.py
@@ -1,20 +1,16 @@
+
+
 go = input("Please enter 16 strings:")
+spList= go.split(" ")
+wordsList= []
 
-def split(go):
-    global smth
-    smth = []
-    smth.append(go)
-    
-split(go)
-
-wordsList= [""]
 spot = 0
 for i in range(0, 4):
-    output = [""]
-    wordsList.append([""])
-    for j in  range(0, 4):
-        wordsList.append(split(spot))
-        output.append(wordsList[i][j]) 
+    output = ""
+    wordsList.append([])
+    for j in range(0, 4):
+        wordsList[i].append(spList[spot])
+        output+= wordsList[i][j] + " "
         spot+= 1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
     print(output)
     
