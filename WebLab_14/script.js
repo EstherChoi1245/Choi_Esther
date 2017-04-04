4b34730e892c550641426a61b2f2e028a8ffd592
Esther Choi <catlover1115@gmail.com>
2017-04-04 10:24:48 -0700

    WebLab_14

--- WebLab_14/script.js
+++ WebLab_14/script.js
@@ -1,6 +1,19 @@
 function validate() {
 	var us = document.forms.input.username.value;
-	// alert(us);
+	var atPos = us.indexOf("@");
+	var dotPos = us.lastIndexOf(".");
+	var pd = document.forms.input.password.value; 
 	
+		
+	if(atPos < 1 || dotPos < atPos+2 || dotPos+2 > us.length)
+		alert("This is not a valid email address!");
+	if(pd.length < 6)
+		alert("Your password does not meet the minimum requirements!");
+	else
+		alert("Success!");
+
+
 	
 	}
+	
+	
