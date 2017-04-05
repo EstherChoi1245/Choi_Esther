667c6d378ed77babd1b221641a6973deb0a21433
Esther Choi <catlover1115@gmail.com>
2017-04-05 10:25:23 -0700

    WebLab_15

--- WebLab_15/brain.js
+++ WebLab_15/brain.js
@@ -1,23 +1,35 @@
 function shapes()	{
 	var x = document.getElementById("canvas");
-	canvas = x.getContext("2d");
+	canvas = x.getContext("2d");	
+	// canvas.addColorStop(0, "blue");
+	// canvas.addColorStop(.5, "white");
+	// canvas.addColorStop(1, "red");	
 	canvas.strokeStyle = "yellow";
-	canvas.fillStyle = "purple";
+	canvas.fillStyle = "red";
 	canvas.beginPath();
-	canvas.moveTo(50, 50);
-	canvas.lineTo(100,100);
-	canvas.lineTo(300, 200);
+
+	canvas.moveTo(780, 1070);
+	canvas.lineTo(895, 1400); 
+	canvas.lineTo(1010, 1080);
+	canvas.lineTo(1340, 1270); 
+	canvas.lineTo(1170, 920);
+	canvas.lineTo(1550, 820); 
+	canvas.lineTo(1180, 690);
+	canvas.lineTo(1390, 382); 
+	canvas.lineTo(1020, 530);
+	canvas.lineTo(905, 200); 
+	canvas.lineTo(790, 520);
+	canvas.lineTo(500, 380);
+	canvas.lineTo(630, 680);
+	canvas.lineTo(330, 795); 
+	canvas.lineTo(620, 910);
+	canvas.lineTo(450, 1225); 
+
+
 	canvas.closePath();
 	canvas.stroke();
 	canvas.fill();
 	
-	
-	// var g = canvas.createLinearGradient(10, 10, 100, 200);
-	// g.addColorStop(0, "blue");
-	// g.addColorStop(.5, "white");
-	// g.addColorStop(1, "red");	
-	// canvas.fillStyle = g;
-	// canvas.fillRect(10, 10, 100, 200);
 	}
 
 window.addEventListener("load", shapes, false);
