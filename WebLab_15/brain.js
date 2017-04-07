8573a9def80874b29daa8eb0042e010834a138da
Esther Choi <catlover1115@gmail.com>
2017-04-07 09:51:22 -0700

    WebLab_15

--- WebLab_15/brain.js
+++ WebLab_15/brain.js
@@ -1,13 +1,9 @@
 function shapes()	{
 	var x = document.getElementById("canvas");
 	canvas = x.getContext("2d");	
-	// canvas.addColorStop(0, "blue");
-	// canvas.addColorStop(.5, "white");
-	// canvas.addColorStop(1, "red");	
-	canvas.strokeStyle = "yellow";
-	canvas.fillStyle = "red";
-	canvas.beginPath();
+	
 
+	canvas.beginPath();
 	canvas.moveTo(780, 1070);
 	canvas.lineTo(895, 1400); 
 	canvas.lineTo(1010, 1080);
@@ -19,16 +15,26 @@ function shapes()	{
 	canvas.lineTo(1020, 530);
 	canvas.lineTo(905, 200); 
 	canvas.lineTo(790, 520);
-	canvas.lineTo(500, 380);
+	canvas.lineTo(450, 370);
 	canvas.lineTo(630, 680);
-	canvas.lineTo(330, 795); 
+	canvas.lineTo(310, 795); 
 	canvas.lineTo(620, 910);
 	canvas.lineTo(450, 1225); 
-
-
 	canvas.closePath();
-	canvas.stroke();
+
+	
+	var g = canvas.createLinearGradient(20, 20, 2000, 2000);
+	g.addColorStop(0, "#DF00FF");
+	g.addColorStop(.2, "#DA70D6");	
+	g.addColorStop(.4, "#DCDCDC");
+	//g.addColorStop(.45, "gray");
+	g.addColorStop(0.85, "#FDFF00");
+	g.addColorStop(0.9, "#FFFF00");
+
+	canvas.fillStyle = g;	
 	canvas.fill();
+	
+
 	
 	}
 
