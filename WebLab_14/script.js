9b4ada10ba7a2a5073e931b207a53780a24f184d
Esther Choi <catlover1115@gmail.com>
2017-05-23 10:20:16 -0700

    WebLab_14

--- WebLab_14/script.js
+++ WebLab_14/script.js
@@ -1,19 +1,22 @@
 function validate() {
 	var us = document.forms.input.username.value;
+	var pd = document.forms.input.password.value; 
 	var atPos = us.indexOf("@");
 	var dotPos = us.lastIndexOf(".");
-	var pd = document.forms.input.password.value; 
 	
 		
-	if(atPos < 1 || dotPos < atPos+2 || dotPos+2 > us.length)
-		alert("This is not a valid email address!");
-	if(pd.length < 6)
-		alert("Your password does not meet the minimum requirements!");
-	else
-		alert("Success!");
+	if((atPos < 1 || dotPos < (atPos + 2))&&(pd.length < 6))
+		alert("This is not a valid email address! The password is too short!");
+	else {
+		if (atPos < 1 || dotPos < atPos + 2) 
+			alert("Invalid email address!");
+		else if (pd.length < 6)			
+			alert("Your password does not meet the minimum requirements!");
+		else 
+			alert("Success!")
 
 
 	
 	}
 	
-	
+}
